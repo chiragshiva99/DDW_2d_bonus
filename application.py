d0ebe88879a6478dfce6e9122e27667a9663d51e
Chirag Shivakumar <86071790+chiragshiva99@users.noreply.github.com>
2021-11-24 21:23:50 +0800

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,4 @@
-from flask import Flask
+from app import application
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello_world():
-    return "<p>DDW EXTRAAAAA</p>"
+if __name__ == "__main__":
+	application.run(host="0.0.0.0", port=8080, debug=True)
