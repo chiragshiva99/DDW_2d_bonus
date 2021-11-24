95d646b81f42d1081f32ba561612a5a89ed59ff9
Chirag Shivakumar <86071790+chiragshiva99@users.noreply.github.com>
2021-11-24 21:22:42 +0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -5,22 +5,22 @@ from app import application
 @application.route('/')
 @application.route('/home')
 def home():
-    return render_template('home.html', title='D2W-Bonus-Home')
+    return render_template('Home.html', title='D2W-Bonus-Home')
 
 
 @application.route('/about')
 def about():
-    return render_template('about.html', title='D2W-Bonus-About')
+    return render_template('About.html', title='D2W-Bonus-About')
 
 
 @application.route('/task1')
 def task1():
-    return render_template('task1.html', title='D2W-Bonus-Task1')
+    return render_template('Task-1.html', title='D2W-Bonus-Task1')
 
 
 @application.route('/task2')
 def task2():
-    return render_template('task2.html', title='D2W-Bonus-Task2')
+    return render_template('Task-2.html', title='D2W-Bonus-Task2')
 
 
 
