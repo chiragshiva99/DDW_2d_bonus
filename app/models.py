8f3ea04c7baffef43f6c762485dc3b7940d88152
Chirag Shivakumar <86071790+chiragshiva99@users.noreply.github.com>
2021-11-25 21:50:44 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,13 @@
 class MLRModel:
-    def __init__(self, num_task: int):
-        if num_task == 1:
-            self.beta0 = -304.993
-            self.beta1 = 0.216613
-            self.beta2 = 336.0383
-            self.beta3 = 0.02595
+    def __init__(self):
+        self.beta0 = -304.993
+        self.beta1 = 0.216613
+        self.beta2 = 336.0383
+        self.beta3 = 0.02595
+
+class LogRegModel:
+    def __init__(self):
+        self.beta0 = 0.12889905
+        self.beta1 = 0.17751605
+        self.beta2 = -0.0794467
+        self.beta3 = 1.18107464
