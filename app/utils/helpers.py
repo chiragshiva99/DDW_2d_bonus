14f472a9b95513b5c78514f5680fb5e576a4aeed
Chirag Shivakumar <86071790+chiragshiva99@users.noreply.github.com>
2021-11-25 21:50:51 +0800

    Update helpers.py

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -19,3 +19,34 @@ def validate_pop_den(pop_den):
         flash('Please choose a range from 100 to 500 for the Population Density')
         return False
     else: return True
+
+def validate_mental_perc(val):
+    if float(val) >= 100 or float(val) < 1 :
+        flash('Please choose a range from 1 to 100 for the Mental Illness Percentage')
+        return False
+    else: return True
+
+def validate_ff_per_hund(val):
+    if float(val) >= 100 or float(val) < 1 :
+        flash('Please choose a range from 1 to 100 for the Number of Fast food restaurants per 100 000 people')
+        return False
+    else: return True
+
+def validate_hypert(val):
+    if float(val) >= 100 or float(val) < 1 :
+        flash('Please choose a range from 1 to 100 for the Hypertension Percentage')
+        return False
+    else: return True
+
+def normalize_z(val, feature_type):
+    if feature_type == 'mental':
+        mean = -6.204714e-16
+        sd = 1.0
+    if feature_type == 'ff':
+        mean = 1.026061e-15
+        sd = 1.0
+    if feature_type == 'hyp':
+        mean = -1.150812e-15
+        sd = 1.0
+
+    return (val - mean) / sd
